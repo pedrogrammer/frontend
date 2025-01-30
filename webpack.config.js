eb4e696b86c41d34afd1a57c9c1d6c005a4793fa
p.sheikhrahimi <pedramshrahimi@gmail.com>
2025-01-30 18:18:10 +0330

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ESLintPlugin({
+      extensions: ["ts", "tsx"], // Lint TypeScript files
+      failOnError: false, // Prevent Webpack from stopping on lint errors
+      emitWarning: true, // Show warnings in terminal
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+    historyApiFallback: true,
+    open: true,
+    hot: true,
+    client: {
+      logging: "error", // Suppresses unnecessary logs
+      overlay: false, // Prevents error overlay in browser
+    },
+  },
+};
