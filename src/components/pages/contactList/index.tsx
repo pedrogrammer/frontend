5cb25e4e43f0727c015efc7d0cb21fafa046b747
p.sheikhrahimi <pedramshrahimi@gmail.com>
2025-01-31 09:34:06 +0330

    chore: add ant design to project

--- src/components/pages/contactList/index.tsx
+++ src/components/pages/contactList/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Input } from "antd";
 
 function ContactList() {
-  return <Link to={`/contact/1`}>Contact #1</Link>;
+  return (
+    <>
+      <Input />
+      <Link to={`/contact/1`}>Contact #1</Link>
+    </>
+  );
 }
 
 export default ContactList;
